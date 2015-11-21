623ff6dd741de86f7bb32233cbc58952749011bc
Aiden Clarke <aidinio64@gmail.com>
2015-11-21 11:52:50 -0500

    save

---
+++ public/js/boot.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Flappy = {};
+
+Flappy.Boot = function() {};
+
+
+Flappy.Boot.prototype = {
+  init: function() {
+  },
+
+  preload: function() {
+    this.game.stage.backgroundColor = "FF0000";
+  },
+
+  create: function() {
+  }
+};
