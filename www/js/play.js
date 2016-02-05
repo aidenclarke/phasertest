f1b1b528df0a3a433d1eb4bc937eacca017dc56e
Aiden Clarke <aidinio64@gmail.com>
2016-02-05 16:43:42 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -2,6 +2,9 @@
 
 'use strict';
 
+var player;
+
+
 Test.Play = function () {};
 
 Test.Play.prototype = {
@@ -11,9 +14,33 @@ Test.Play.prototype = {
 
   create: function () {
     this.background = null;
+    game.add.sprite(0,0,'player');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
   },
 
   update: function () {
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
+
+
+
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+      player.body.velocity.y = -300;
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+       player.body.velocity.y = 300;
+     }
+
+     if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+       player.body.velocity.x = -300;
+     }
+
+     if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+       player.body.velocity.x = 300;
+     }
   },
 
   end: function () {
--- www/js/preload.js
+++ www/js/preload.js
@@ -9,6 +9,7 @@ Test.Preload.prototype = {
 
   preload: function () {
     this.load.spritesheet('background','assets/background.jpg',600,600);
+    this.load.spritesheet('player','assets/player.jpg',130,225);
   },
 
   create: function () {
